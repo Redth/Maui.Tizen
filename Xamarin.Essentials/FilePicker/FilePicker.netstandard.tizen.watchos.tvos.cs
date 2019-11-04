11a1cd214a1380b83bdb472cfc4cc5b44752f0c4
Michael Fink <michael.fink@asamnet.de>
2019-11-04 19:34:37 +0100

    fixed compiling for tizen, watchos and tvos targets

---
+++ Xamarin.Essentials/FilePicker/FilePicker.netstandard.tizen.watchos.tvos.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials
+{
+    public static partial class FilePicker
+    {
+        static Task<PickResult> PlatformPickFileAsync(PickOptions options)
+            => throw new NotImplementedInReferenceAssemblyException();
+    }
+
+    public partial class FilePickerFileType
+    {
+        public static FilePickerFileType PlatformImageFileType()
+            => throw new NotImplementedInReferenceAssemblyException();
+
+        public static FilePickerFileType PlatformPngFileType()
+            => throw new NotImplementedInReferenceAssemblyException();
+    }
+
+    public partial class PickResult
+    {
+        Stream PlatformGetStream() =>
+            throw new NotImplementedInReferenceAssemblyException();
+    }
+}
